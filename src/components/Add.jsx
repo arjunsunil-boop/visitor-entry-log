a95cd7ccf85dc6997f680a591afb35154476846b
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-08-27 23:40:36 +0530

    Initial commit

---
+++ src/components/Add.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+
+        <div className="container">
+                
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/components/View.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div>
+
+        <div className="container">
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default View
