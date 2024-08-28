2610fe9143e03caa978b6d2f8e71557f4e7f5d8f
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-08-28 20:36:19 +0530

    View

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -4,10 +4,37 @@ const View = () => {
   return (
     <div>
 
-        <div className="container">
+      <div className="container">
 
-            
+        <div className="row g-3">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+            <table class="table">
+              <thead>
+                <tr>
+                 
+                  <th scope="col">Name</th>
+                  <th scope="col">Age</th>
+                  <th scope="col">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  
+                  <td>Mark</td>
+                  <td>23</td>
+                  <td>12-02-2023</td>
+                </tr>
+ 
+              </tbody>
+            </table>
+
+
+          </div>
         </div>
+
+
+      </div>
     </div>
   )
 }
