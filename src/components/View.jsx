d1b2d8315a9d730779c69b841c0e25a925995bb4
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-08-28 22:51:18 +0530

    View final with local api

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from './Nav'
+import axios from 'axios'
 
 const View = () => {
 
   const [view, ChangeView] = useState(
-    [
 
-      {
-        "name": "John",
-        "age": "30",
-        "date": "01-01-2022"
-      },
-      {
-        "name": "Jane",
-        "age": "25",
-        "date": "02-02-2022"
-      },
-      {
-        "name": "Mike",
-        "age": "35",
-        "date": "03-03-2022"
-      }
-    ]
+    []
 
-    
+
   )
+
+  const fetch = () => {
+
+    axios.get("http://127.0.0.1:5000/users").then(
+
+      (ItIsTheResponse) => {
+
+        ChangeView(ItIsTheResponse.data)
+
+
+      }
+    ).catch()
+
+
+  }
+
+  useEffect(
+    () => {
+      fetch()
+    }, []
+  )
+
+
   return (
     <div>
-      <Nav/>
+      <Nav />
       <br />
 
       <div className="container">
@@ -35,38 +43,41 @@ const View = () => {
         <div className="row g-3">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-            <table class="table">
+            <table className="table">
               <thead>
                 <tr>
-                 
+
                   <th scope="col">Name</th>
                   <th scope="col">Age</th>
                   <th scope="col">Date</th>
+                  <th scope="col">email</th>
                 </tr>
               </thead>
               <tbody>
                 {
 
                   view.map(
-                  (i,index)=>{
+                    (i, index) => {
 
-                    return(
+                      return (
 
-                      <tr>
-                      <td>{i.name}</td>
-                      <td>{i.age}</td>
-                      <td>{i.date}</td>
-                    </tr>
-                    )
+                        <tr>
+                          <td>{i.name}</td>
+                          <td>{i.age}</td>
+                          <td>{i.date}</td>
+                          <td>{i.email}</td>
+
+                        </tr>
+                      )
 
 
-                  }
+                    }
                   )
                 }
 
-             
 
- 
+
+
               </tbody>
             </table>
 
