555b01a578c61e06a8ce4fdd29212413e48a9c33
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-08-28 20:26:24 +0530

    Add visitor

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,11 +4,38 @@ const Add = () => {
   return (
     <div>
 
-        <div className="container">
-                
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col md-6 col lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Name</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col md-6 col lg-6 col-xl-6 col-xxl-6">
+              <label htmlFor="" className="form-label">Age</label>
+              <input type="text" className="form-control" />
 
+              </div>
+              <div className="col col-12 col-sm-6 col md-6 col lg-6 col-xl-6 col-xxl-6">
+              <label htmlFor="" className="form-label">Email</label>
+              <input type="text" className="form-control" />
 
+              </div>
+              <div className="col col-12 col-sm-6 col md-6 col lg-6 col-xl-6 col-xxl-6">
+              <label htmlFor="" className="form-label">Date</label>
+              <input type="date" className="form-control" />
+
+              </div>
+              <div className="col col-12 col-sm-6 col md-6 col lg-6 col-xl-6 col-xxl-6">
+              <button className="btn btn-success">Submit</button>
+
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
     </div>
   )
 }
