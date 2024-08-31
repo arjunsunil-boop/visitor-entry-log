13b9db7dc3095b1ceda68b22eb32799abbca4170
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-08-31 17:20:17 +0530

    minor css fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div>
         <Nav/>
-        <div className="container">
+        <div className="container" id='login-container'>
             <div className="row">
                 <div className="col col-12 col-12 col-sm-12 col-md-12 col-lg-12 co-xl-12 col-xxl-12">
                     <br />
